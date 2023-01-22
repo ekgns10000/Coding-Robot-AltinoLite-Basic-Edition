533616e2363f5ffa14d8d2fd571d2ba9859f524b
ekgns10000 <120773998+ekgns10000@users.noreply.github.com>
2023-01-22 17:21:54 +0900

    Section 3 - 알티노 라이트 방향 설정하기
    2023-01-15에 진행
    2023-01-21에 복습

---
+++ Book1 Section3/Book1 Section3 Num2.py
@@ -0,0 +1,15 @@
+from AltinoLite import*
+
+Open()
+
+steering(32)
+delay(1000)
+steering(63)
+delay(1000)
+steering(95)
+delay(1000)
+steering(127)
+delay(1000)
+steering(0)
+
+close()
---
+++ Book1 Section3/Book1 Section3 Num5.py
@@ -0,0 +1,16 @@
+from AltinoLite import*
+
+Open()
+
+go(300,300)
+steering(-127)
+delay(1000)
+
+go(0,0)
+delay(3000)
+
+steering(0)
+delay(1000)
+
+
+close()
